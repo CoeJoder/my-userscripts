90eeb9cfd4c641bfd9cb62b367e6a9c01cc38123
CoeJoder <CoeJoder@users.noreply.github.com>
2020-05-07 15:59:03 -0700

    Create youtube anti-bigbrother userscript

---
+++ youtube_remove_propaganda_box.tamper.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         YouTube: remove propaganda box
+// @namespace    https://www.youtube.com
+// @version      0.1
+// @description  Removes the propaganda box that Big Brother places under "controversial" videos.
+// @author       CoeJoder
+// @match        *://www.youtube.com/watch?*
+// @grant        none
+// ==/UserScript==
+
+const box = document.querySelector("#clarify-box");
+if (box) {
+	box.remove();
+}
