f2eea70f6a50625e1ebde522a972e731373901b7
CoeJoder <CoeJoder@users.noreply.github.com>
2020-11-07 11:53:16 -0800

    Improve youtube propaganda busting script

--- youtube_remove_propaganda_box.tamper.js
+++ youtube_remove_propaganda_box.tamper.js
@@ -5,10 +5,22 @@
 // @description  Removes the propaganda box that Big Brother places under "controversial" videos.
 // @author       CoeJoder
 // @match        *://www.youtube.com/watch?*
+// @require      https://cdn.jsdelivr.net/gh/CoeJoder/GM_wrench@v1.1/dist/GM_wrench.min.js
 // @grant        none
 // ==/UserScript==
 
-const box = document.querySelector("#clarify-box");
+const PROPAGANDA_BOX_CSS = "#clarify-box";
+
+// remove it initially right away
+const box = document.querySelector(PROPAGANDA_BOX_CSS);
 if (box) {
     box.remove();
 }
+
+// box might reappear due to in-page navigation,
+// check every 3 seconds for it to reappear
+const waitOnce = false;
+const interval = 3000;
+GM_wrench.waitForKeyElements(PROPAGANDA_BOX_CSS, (box) => {
+    box.remove();
+}, waitOnce, interval);
