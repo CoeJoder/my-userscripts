aca13764ed8914e971aa5d5a2617413042a9a9e5
CoeJoder <CoeJoder@users.noreply.github.com>
2024-02-09 15:07:11 -0800

    Add Yahoo mail remove search title

---
+++ yahoomail_remove_searchtitle.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Yahoo! mail remove search title
+// @namespace    https://github.com/CoeJoder/my-userscripts
+// @version      0.1
+// @description  Removes the magnifying glass popup when hoving an email subject
+// @author       CoeJoder
+// @match        https://mail.yahoo.com/d/*
+// @icon         https://www.google.com/s2/favicons?domain=mail.yahoo.com
+// @require      https://cdn.jsdelivr.net/gh/CoeJoder/GM_wrench@v1.4/dist/GM_wrench.min.js
+// ==/UserScript==
+(async function ({ addCss }) {
+	addCss(`
+	button[title="Search for messages with this subject"] {
+		display: none !important;
+	}
+	`);
+})(GM_wrench);
