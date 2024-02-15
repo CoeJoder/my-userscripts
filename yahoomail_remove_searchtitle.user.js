05463ea876602cc58be7a2462455dea7b4b91a89
CoeJoder <CoeJoder@users.noreply.github.com>
2024-02-14 17:52:18 -0800

    Update yahoomail URL

--- yahoomail_remove_searchtitle.user.js
+++ yahoomail_remove_searchtitle.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  Removes the magnifying glass popup when hoving an email subject
 // @author       CoeJoder
-// @match        https://mail.yahoo.com/d/*
+// @match        https://mail.yahoo.com/*
 // @icon         https://www.google.com/s2/favicons?domain=mail.yahoo.com
 // @require      https://cdn.jsdelivr.net/gh/CoeJoder/GM_wrench@v1.4/dist/GM_wrench.min.js
 // ==/UserScript==
