62e2ce7d1063ddeee61b30186434f77451b1720b
CoeJoder <CoeJoder@users.noreply.github.com>
2024-02-14 20:46:38 -0800

    Increment violentmonkey test script

--- violentmonkey_test.user.js
+++ violentmonkey_test.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://github.com/CoeJoder/my-userscripts
 // @homepageURL  https://github.com/CoeJoder/my-userscripts/blob/master/violentmonkey_test.user.js
 // @downloadURL  https://cdn.jsdelivr.net/gh/CoeJoder/my-userscripts/violentmonkey_test.user.js
-// @version      0.2
+// @version      0.3
 // @description  Test to see if violentmonkey will check the @version when @downloadURL is set from a manually entered script
 // @author       CoeJoder
 // @match        https://github.com/
