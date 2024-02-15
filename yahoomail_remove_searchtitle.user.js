2659f4640d12e0f4657e3ae92a7768d5a83207b7
CoeJoder <CoeJoder@users.noreply.github.com>
2024-02-14 18:19:50 -0800

    Add metadata to script

--- yahoomail_remove_searchtitle.user.js
+++ yahoomail_remove_searchtitle.user.js
@@ -1,8 +1,10 @@
 // ==UserScript==
 // @name         Yahoo! mail remove search title
 // @namespace    https://github.com/CoeJoder/my-userscripts
-// @version      0.1
-// @description  Removes the magnifying glass popup when hoving an email subject
+// @homepageURL  https://github.com/CoeJoder/my-userscripts/blob/master/yahoomail_remove_searchtitle.user.js
+// @downloadURL  https://cdn.jsdelivr.net/gh/CoeJoder/my-userscripts/yahoomail_remove_searchtitle.user.js
+// @version      0.2
+// @description  Removes the magnifying glass popup when hovering an email subject
 // @author       CoeJoder
 // @match        https://mail.yahoo.com/*
 // @icon         https://www.google.com/s2/favicons?domain=mail.yahoo.com
