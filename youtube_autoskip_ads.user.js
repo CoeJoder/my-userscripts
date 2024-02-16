bc56b8a83599bfd1fec9f91191143bda579acd19
CoeJoder <CoeJoder@users.noreply.github.com>
2024-02-16 03:31:43 -0800

    Update YouTube: autoskip ads

--- youtube_autoskip_ads.user.js
+++ youtube_autoskip_ads.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://github.com/CoeJoder/my-userscripts
 // @homepageURL  https://github.com/CoeJoder/my-userscripts/blob/master/youtube_autoskip_ads.user.js
 // @downloadURL  https://cdn.jsdelivr.net/gh/CoeJoder/my-userscripts/youtube_autoskip_ads.user.js
-// @version      0.1
+// @version      0.2
 // @description  Stealthily skips skippable ads on YouTube videos.
 // @author       CoeJoder
 // @match        *://www.youtube.com/watch?*
@@ -12,57 +12,63 @@
 // @grant        none
 // ==/UserScript==
 
-(async function({ waitForKeyElements, wait }) {
+(async function({ sleep, wait, until, By }) {
 
-	const tag = (str) => `[${GM_info.script.name}] ${str}`;
-	const log = (str) => console.log(tag(str));
-	log("Script loaded");
+  const DEBUG = true;
+  const tag = (str) => `[${GM_info.script.name}] ${str}`;
+  const log = (str) => console.log(tag(str));
+  const debug = (str) => DEBUG ? console.log(tag(str)) : undefined;
+  const error = (str) => console.error(tag(str));
+  log("Userscript loaded");
 
-	// Strategy:
-	// Although the 'Skip' button exists immediately once an ad begins playing,
-	// wait until it is displayed to click it, so as to avoid adblock detection.
-	//   [outer loop] wait for the 'Skip' button to exist
-	//   [inner loop] wait for the 'Skip' button to be visible
+  // Strategy:
+  // Although the 'Skip' button exists immediately once an ad begins playing,
+  // wait until it is displayed to click it, so as to avoid adblock detection.
 
-	// When ad appears, countdown until 'Skip' displays is about 5 seconds.
-	// Outer-loop < 2x countdown
-	// Outer-loop > inner-loop > countdown 
-	const OUTER_INTERVAL = 7000;
-	const INNER_INTERVAL = 6000;
-	const INNER_POLL = 100;
+  const POLL_INTERVAL_LONG = 5000;
+  const POLL_INTERVAL_SHORT = 500;
+  const AD_SKIP_BUTTON_CSS = ".ytp-ad-skip-button-text";
+  const AD_SKIP_BUTTON_CONTAINER_CSS = ".ytp-ad-skip-button-container";
+  const MAX_ERRORS = 3;
 
-	// handle ads throughout the video, not just the first one
-	const OUTER_JUST_ONCE = false;
-	
-	// `display` is actually toggled on 'Skip' button's container
-	const isDisplayed = (skipButtonContainer) => 
-			window.getComputedStyle(skipButtonContainer).display !== "none";
-	
-	// CSS selectors
-	const AD_SKIP_BUTTON_CSS = ".ytp-ad-skip-button-text";
-	const AD_SKIP_BUTTON_CONTAINER_CSS = ".ytp-ad-skip-button-container";
-	
-	// outer-wait for an ad to appear, repeated indefinitely
-	waitForKeyElements(AD_SKIP_BUTTON_CONTAINER_CSS, (skipButtonContainer) => {
+  const isSkipButtonDisplayed = () => {
+    debug("Waiting for 'Skip' to be displayed...");
+    // check 'Skip' button's container for visibility
+    const skipButtonContainer = document.querySelector(AD_SKIP_BUTTON_CONTAINER_CSS);
+    return skipButtonContainer !== null &&
+      	window.getComputedStyle(skipButtonContainer).display !== "none";
+  };
 
-		log("Ad encountered.  Waiting for 'Skip' button to appear...");
-		// inner-wait for the 'Skip' button to be visible, once
-		wait({
-			condition: isDisplayed,
-			input: skipButtonContainer,
-			timeout: INNER_INTERVAL,
-			pollTimeout: INNER_POLL,
-			message: tag("Timed out waiting for 'Skip' button to appear"),
-		}).then(() => {
-			const skipButton = document.querySelector(AD_SKIP_BUTTON_CSS);
-			if (skipButton === null) {
-				throw new Error(tag("'Skip' button not found"));
-			}
-			log("Clicking 'Skip' button...");
-			skipButton.click();
-		});
+  // stay running to handle all ads; abort if too many errors
+  let errCount = 0;
+  while (errCount < MAX_ERRORS) {
+    try {
+      // long-poll until ad is detected
+      const skipButton = await wait({
+        condition: until.elementLocated(By.css(AD_SKIP_BUTTON_CSS)),
+        pollTimeout: POLL_INTERVAL_LONG,
+      });
 
-	}, OUTER_JUST_ONCE, OUTER_INTERVAL);
+      debug("Ad found. Waiting for 'Skip' to be displayed...");
+      // short-poll until 'Skip' is displayed
+      await wait({
+        condition: isSkipButtonDisplayed,
+        pollTimeout: POLL_INTERVAL_SHORT,
+      });
+      
+      if (skipButton === null) {
+        throw new Error(tag("'Skip' button not found"));
+      }
+      debug("Clicking 'Skip' button...");
+      skipButton.click();
+    } catch (e) {
+      error(e.message);
+      errCount++;
+    }
 
+    // throttling
+    await sleep(POLL_INTERVAL_LONG);
+  }
+  error(`Exceeded max-error count (${errCount}). Aborting...`);
 })(GM_wrench);
 
