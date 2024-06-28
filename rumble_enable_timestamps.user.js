1912e9bd9dba3a3b64a31a0727ff8b1481a1c8e3
CoeJoder <CoeJoder@users.noreply.github.com>
2024-06-28 03:02:27 -0700

    Update rumble script

--- rumble_enable_timestamps.user.js
+++ rumble_enable_timestamps.user.js
@@ -3,35 +3,33 @@
 // @namespace    https://github.com/CoeJoder/my-userscripts
 // @homepageURL  https://github.com/CoeJoder/my-userscripts/blob/master/rumble_enable_timestamps.user.js
 // @downloadURL  https://cdn.jsdelivr.net/gh/CoeJoder/my-userscripts/rumble_enable_timestamps.user.js
-// @version      0.1
+// @version      0.2
 // @description  Turn timestamps in the comment section into seekable links, similar to how YouTube does it.
 // @author       CoeJoder
 // @match        *://rumble.com/*.html
-// @require      https://cdn.jsdelivr.net/gh/CoeJoder/GM_wrench@v1.5/dist/GM_wrench.min.js
+// @require      https://cdn.jsdelivr.net/npm/@violentmonkey/dom@2
 // @icon         https://www.google.com/s2/favicons?domain=rumble.com
 // ==/UserScript==
 
-(() => {
-    const commentsSelector = ".comment-text";
-    const waitOnce = true;
-    const interval = 500;
-
-    // replaces the text timestamps in comments with seek-buttons
-    const processComment = (replayComment) => {
-        let replaced = false;
-        const html = replayComment.innerHTML.replace(/(?:(\d+):)?(\d+):(\d+)/, (match, h, m, s, offset, string) => {
-            replaced = true;
-            h = parseInt(h || "0");
-            m = parseInt(m);
-            s = parseInt(s);
-            const seekTo = h*3600 + m*60 + s;
-            return `<a href="#" style='color: #37c;' onclick="const vid = document.querySelector('video');
-                    vid.muted = false; vid.volume = 1; vid.currentTime = ${seekTo}; vid.play();">${match}</a>`;
-        });
-        if (replaced) {
-            replayComment.innerHTML = html;
-        }
-    };
-
-    GM_wrench.waitForKeyElements(commentsSelector, processComment, waitOnce, interval);
-})();
+VM.observe(document.body, () => {
+  const comments = document.querySelectorAll('.comment-text');
+  if (comments.length > 0) {
+    comments.forEach(comment => {
+    let replaced = false;
+      const html = comment.innerHTML.replace(/(?:(\d+):)?(\d+):(\d+)/, (match, h, m, s, offset, string) => {
+        replaced = true;
+        h = parseInt(h || "0");
+        m = parseInt(m);
+        s = parseInt(s);
+        const seekTo = h*3600 + m*60 + s;
+        return `<a href="#" style='color: #37c;' onclick="const vid = document.querySelector('video');
+            vid.muted = false; vid.volume = 1; vid.currentTime = ${seekTo}; vid.play();">${match}</a>`;
+      });
+      if (replaced) {
+        comment.innerHTML = html;
+      }
+    });
+    // disconnect observer
+    return true;
+  }
+});
