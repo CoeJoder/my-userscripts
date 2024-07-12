bdd63b8ac2140551f080fe3839a7ca8f6de70a88
CoeJoder <CoeJoder@users.noreply.github.com>
2024-07-12 02:34:45 -0700

    Fix YouTube: remove propaganda box

--- youtube_remove_propaganda_box.user.js
+++ youtube_remove_propaganda_box.user.js
@@ -1,28 +1,43 @@
 // ==UserScript==
-// @name         YouTube: remove propaganda box
-// @namespace    https://github.com/CoeJoder/my-userscripts
-// @homepageURL  https://github.com/CoeJoder/my-userscripts/blob/master/youtube_remove_propaganda_box.user.js
-// @downloadURL  https://cdn.jsdelivr.net/gh/CoeJoder/my-userscripts/youtube_remove_propaganda_box.user.js
-// @version      0.3
-// @description  Removes the propaganda box that Big Brother places under "controversial" videos.
-// @author       CoeJoder
-// @match        *://www.youtube.com/watch?*
-// @require      https://cdn.jsdelivr.net/gh/CoeJoder/GM_wrench@v1.1/dist/GM_wrench.min.js
-// @grant        none
+// @name        YouTube: remove propaganda box
+// @namespace   https://github.com/CoeJoder/my-userscripts
+// @homepageURL https://github.com/CoeJoder/my-userscripts/blob/master/youtube_remove_propaganda_box.user.js
+// @downloadURL https://cdn.jsdelivr.net/gh/CoeJoder/my-userscripts/youtube_remove_propaganda_box.user.js
+// @version     0.4
+// @description Removes the propaganda box that Big Brother places under "controversial" videos.
+// @author      CoeJoder
+// @match       *://www.youtube.com/watch?*
+// @icon        https://www.google.com/s2/favicons?domain=youtube.com
+// @require     https://cdn.jsdelivr.net/gh/CoeJoder/GM_wrench@v1.5/dist/GM_wrench.min.js
+// @require     https://cdn.jsdelivr.net/npm/@violentmonkey/dom@2
+// @grant       none
 // ==/UserScript==
 
-const PROPAGANDA_BOX_CSS = "#clarify-box";
+// Strategy: wait for the container to load, then remove propaganda box if
+// present, then attach a permanent listener to the container which deletes
+// the propaganda if it returns e.g. while navigating to other videos.
 
-// remove it initially right away
-const box = document.querySelector(PROPAGANDA_BOX_CSS);
-if (box) {
-    box.remove();
-}
+const CONTAINER_CSS = "#clarify-box";
+const PROPAGANDA_BOX_CSS = "ytd-clarification-renderer";
 
-// box might reappear due to in-page navigation,
-// check every 3 seconds for it to reappear
-const waitOnce = false;
-const interval = 3000;
-GM_wrench.waitForKeyElements(PROPAGANDA_BOX_CSS, (box) => {
-    box.remove();
-}, waitOnce, interval);
+(async ({wait}) => {
+  const container = await wait({
+    condition: () => document.querySelector(CONTAINER_CSS),
+    timeout: 75 * 1000,
+    pollTimeout: 1000,
+    message: `Timed out waiting for "${CONTAINER_CSS}"`,
+  });
+
+  const removeIfPresent = () => {
+    const propagandaBox = container.querySelector(PROPAGANDA_BOX_CSS);
+    if (propagandaBox) {
+      propagandaBox.remove();
+    }
+  };
+
+  removeIfPresent();
+  
+  VM.observe(container, () => {
+    removeIfPresent();
+  }, {subtree: false});
+})(GM_wrench);
