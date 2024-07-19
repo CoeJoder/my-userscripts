4b6c853f9e506b677797949eaa4955052a7ee585
CoeJoder <CoeJoder@users.noreply.github.com>
2024-07-18 18:50:14 -0700

    Update nitter script

--- nitter_redirect.user.js
+++ nitter_redirect.user.js
@@ -4,11 +4,15 @@
 // @homepageURL  https://github.com/CoeJoder/my-userscripts/blob/master/nitter_redirect.user.js
 // @downloadURL  https://cdn.jsdelivr.net/gh/CoeJoder/my-userscripts/nitter_redirect.user.js
 // @description  Always redirects to nitter.net
-// @include      *://twitter.com/*
-// @version      1.1
+// @match        https://x.com/*
+// @match        https://twitter.com/*
+// @version      1.2
 // @run-at       document-start
 // @author       CoeJoder
 // @grant        none
 // ==/UserScript==
 
-window.location.replace("https://nitter.moomoo.me" + window.location.pathname + window.location.search);
+// this seems to be the last working Nitter instance...
+const NITTER_INSTANCE = 'https://nitter.poast.org'
+
+window.location.replace(NITTER_INSTANCE + window.location.pathname + window.location.search);
