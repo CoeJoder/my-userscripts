aac5074a1c004f0ff2a9dddadf9e127ea636460c
CoeJoder <CoeJoder@users.noreply.github.com>
2025-01-25 18:23:44 -0800

    Update nitter userscript

--- nitter_redirect.user.js
+++ nitter_redirect.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://github.com/CoeJoder/my-userscripts
 // @homepageURL  https://github.com/CoeJoder/my-userscripts/blob/master/nitter_redirect.user.js
 // @downloadURL  https://cdn.jsdelivr.net/gh/CoeJoder/my-userscripts/nitter_redirect.user.js
-// @description  Always redirects to nitter.net
+// @description  Redirect all X/Twitter links through a Nitter instance
 // @match        https://x.com/*
 // @match        https://twitter.com/*
 // @version      1.2
@@ -12,7 +12,11 @@
 // @grant        none
 // ==/UserScript==
 
-// this seems to be the last working Nitter instance...
-const NITTER_INSTANCE = 'https://nitter.poast.org'
+// known working Nitter instances
+const NITTER_POAST_ORG = 'https://nitter.poast.org';
+const NITTER_XCANCEL_COM = 'https://xcancel.com';
 
-window.location.replace(NITTER_INSTANCE + window.location.pathname + window.location.search);
+// xcancel.com seems quickest and works with embedded media
+const SELECTED_NITTER_INSTANCE = NITTER_XCANCEL_COM;
+
+window.location.replace(SELECTED_NITTER_INSTANCE + window.location.pathname + window.location.search);
