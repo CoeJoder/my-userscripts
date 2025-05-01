72d0b41c00d6d1b87c70f2ab6ec1bebf41f52590
CoeJoder <CoeJoder@users.noreply.github.com>
2025-05-01 01:36:31 -0700

    Add `google-voice` userscript

---
+++ google-voice.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Google Voice
+// @namespace    https://github.com/CoeJoder/my-userscripts
+// @homepageURL  https://github.com/CoeJoder/my-userscripts/blob/master/google-voice.user.js
+// @downloadURL  https://cdn.jsdelivr.net/gh/CoeJoder/my-userscripts/google-voice.user.js
+// @version      0.1
+// @description  Applies a dark theme based on google.com results style.
+// @author       CoeJoder
+// @match        *://voice.google.com/u/*
+// @icon         https://fonts.gstatic.com/s/i/productlogos/voice_2020q4/v1/web-32dp/logo_voice_2020q4_color_1x_web_32dp.png
+// @resource     IMPORTED_CSS https://cdn.jsdelivr.net/gh/CoeJoder/my-userscripts/google-voice.user.js.css
+// @grant        GM_getResourceText
+// @grant        GM_addStyle
+// ==/UserScript==
+GM_addStyle(GM_getResourceText('IMPORTED_CSS'));
