171ac973cb3c52212471b8f0cfb4494eb5858d46
CoeJoder <CoeJoder@users.noreply.github.com>
2025-05-01 04:25:12 -0700

    Update `google-voice` style

--- google-voice.user.js
+++ google-voice.user.js
@@ -6,10 +6,14 @@
 // @version      0.1
 // @description  Applies a dark theme based on google.com results style.
 // @author       CoeJoder
-// @match        *://voice.google.com/u/*
+// @match        *://voice.google.com/u/0/calls
+// @match        *://voice.google.com/u/0/messages
+// @match        *://voice.google.com/u/0/voicemail
 // @icon         https://fonts.gstatic.com/s/i/productlogos/voice_2020q4/v1/web-32dp/logo_voice_2020q4_color_1x_web_32dp.png
 // @resource     IMPORTED_CSS https://cdn.jsdelivr.net/gh/CoeJoder/my-userscripts/google-voice.user.js.css
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
+// @run-at       document-body
 // ==/UserScript==
+
 GM_addStyle(GM_getResourceText('IMPORTED_CSS'));
