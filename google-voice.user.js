f2989d5bcabf3e0f65e0e6ebf59d0ca1cb7928a6
CoeJoder <CoeJoder@users.noreply.github.com>
2025-05-02 01:45:22 -0700

    Update `google-voice` script

--- google-voice.user.js
+++ google-voice.user.js
@@ -6,9 +6,7 @@
 // @version      0.1
 // @description  Applies a dark theme based on google.com results style.
 // @author       CoeJoder
-// @match        *://voice.google.com/u/0/calls
-// @match        *://voice.google.com/u/0/messages
-// @match        *://voice.google.com/u/0/voicemail
+// @match        *://voice.google.com/*
 // @icon         https://fonts.gstatic.com/s/i/productlogos/voice_2020q4/v1/web-32dp/logo_voice_2020q4_color_1x_web_32dp.png
 // @resource     IMPORTED_CSS https://cdn.jsdelivr.net/gh/CoeJoder/my-userscripts/google-voice.user.js.css
 // @grant        GM_getResourceText
@@ -16,4 +14,37 @@
 // @run-at       document-body
 // ==/UserScript==
 
-GM_addStyle(GM_getResourceText('IMPORTED_CSS'));
+const IMPORTED_CSS = GM_getResourceText('IMPORTED_CSS');
+
+const TARGET_PATHNAMES = Object.freeze([
+  '/u/0/',
+  '/u/0/calls',
+  '/u/0/messages',
+  '/u/0/voicemail',
+]);
+
+let styleElm = null;
+const onPathnameChange = () => {
+  if (TARGET_PATHNAMES.includes(document.location.pathname)) {
+    if (styleElm === null) {
+      styleElm = GM_addStyle(IMPORTED_CSS);
+    }
+  } else {
+    styleElm?.remove();
+    styleElm = null;
+  }
+};
+
+onPathnameChange();
+
+let oldPathname = document.location.pathname;
+const observer = new MutationObserver(_ => {
+  if (oldPathname !== document.location.pathname) {
+    oldPathname = document.location.pathname;
+    onPathnameChange();
+  }
+});
+observer.observe(document.querySelector('body'), {
+  childList: true,
+  subtree: true,
+})
