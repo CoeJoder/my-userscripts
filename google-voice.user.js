529fb1e3e692608ed653d212a742723093ea59de
CoeJoder <CoeJoder@users.noreply.github.com>
2025-05-02 02:18:21 -0700

    Update `google-voice` script

--- google-voice.user.js
+++ google-voice.user.js
@@ -3,17 +3,16 @@
 // @namespace    https://github.com/CoeJoder/my-userscripts
 // @homepageURL  https://github.com/CoeJoder/my-userscripts/blob/master/google-voice.user.js
 // @downloadURL  https://cdn.jsdelivr.net/gh/CoeJoder/my-userscripts/google-voice.user.js
-// @version      0.1
+// @version      0.2
 // @description  Applies a dark theme based on google.com results style.
 // @author       CoeJoder
 // @match        *://voice.google.com/*
 // @icon         https://fonts.gstatic.com/s/i/productlogos/voice_2020q4/v1/web-32dp/logo_voice_2020q4_color_1x_web_32dp.png
-// @resource     IMPORTED_CSS https://cdn.jsdelivr.net/gh/CoeJoder/my-userscripts/google-voice.user.js.css
+// @resource     IMPORTED_CSS https://cdn.jsdelivr.net/gh/CoeJoder/my-userscripts@171ac97/google-voice.user.js.css
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
 // @run-at       document-body
 // ==/UserScript==
-
 const IMPORTED_CSS = GM_getResourceText('IMPORTED_CSS');
 
 const TARGET_PATHNAMES = Object.freeze([
