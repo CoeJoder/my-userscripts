9eed2de1abda6be3b27a54b0c1464d75c28e292a
CoeJoder <CoeJoder@users.noreply.github.com>
2025-09-03 22:16:54 -0700

    Add `biblegateway` script

---
+++ biblegateway.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        BibleGateway
+// @namespace   https://github.com/CoeJoder/my-userscripts
+// @homepageURL https://github.com/CoeJoder/my-userscripts/blob/master/biblegateway.user.js
+// @downloadURL https://cdn.jsdelivr.net/gh/CoeJoder/my-userscripts/biblegateway.user.js
+// @version     0.1
+// @description Dark mode for biblegateway.com
+// @author      CoeJoder
+// @match       *://www.biblegateway.com/*
+// @icon        https://www.google.com/s2/favicons?domain=biblegateway.com/
+// @resource    IMPORTED_CSS https://cdn.jsdelivr.net/gh/CoeJoder/my-userscripts@f3063fa2/biblegateway.user.js.css
+// @grant       GM_getResourceText
+// @grant       GM_addStyle
+// @run-at      document-body
+// ==/UserScript==
+
+GM_addStyle(GM_getResourceText('IMPORTED_CSS'));
