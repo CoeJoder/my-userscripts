742ca38c16ddadb074f428410a4c3b42e91de4ae
CoeJoder <CoeJoder@users.noreply.github.com>
2026-06-04 18:30:34 -0700

    Add `deepl` script

---
+++ deepl.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        DeepL
+// @namespace   https://github.com/CoeJoder/my-userscripts
+// @homepageURL https://github.com/CoeJoder/my-userscripts/blob/master/deepl.user.js
+// @downloadURL https://cdn.jsdelivr.net/gh/CoeJoder/my-userscripts/deepl.user.js
+// @version     0.1
+// @description Dark mode for deepl.com
+// @author      CoeJoder
+// @match       *://www.deepl.com/*
+// @icon        https://www.google.com/s2/favicons?domain=deepl.com/
+// @resource    IMPORTED_CSS https://cdn.jsdelivr.net/gh/CoeJoder/my-userscripts@4e2e0d06/deepl.user.js.css
+// @grant       GM_getResourceText
+// @grant       GM_addStyle
+// @run-at      document-body
+// ==/UserScript==
+
+GM_addStyle(GM_getResourceText('IMPORTED_CSS'));
