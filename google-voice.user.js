5157fcde5278dab377cb9cb2b2beac91cdffb872
CoeJoder <CoeJoder@users.noreply.github.com>
2026-06-17 19:32:15 -0700

    Update `google-voice` script

--- google-voice.user.js
+++ google-voice.user.js
@@ -4,13 +4,14 @@
 // @homepageURL  https://github.com/CoeJoder/my-userscripts/blob/master/google-voice.user.js
 // @downloadURL  https://cdn.jsdelivr.net/gh/CoeJoder/my-userscripts/google-voice.user.js
 // @version      0.4
-// @description  Forcibly enables the hidden "dark theme" feature of Google Voice.
+// @description  Forcibly enables the hidden "dark theme" feature of Google Voice, plus some customizations.
 // @author       CoeJoder
 // @match        *://voice.google.com/*
 // @icon         https://fonts.gstatic.com/s/i/productlogos/voice_2020q4/v1/web-32dp/logo_voice_2020q4_color_1x_web_32dp.png
-// @resource     IMPORTED_CSS https://cdn.jsdelivr.net/gh/CoeJoder/my-userscripts@9c7049c4/google-voice.user.js.css
+// @resource     IMPORTED_CSS https://cdn.jsdelivr.net/gh/CoeJoder/my-userscripts@61ab6d0b/google-voice.user.js.css
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
 // @run-at       document-body
 // ==/UserScript==
+
 GM_addStyle(GM_getResourceText('IMPORTED_CSS'));
